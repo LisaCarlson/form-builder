65d58a3e854cb21a63c0d083eaa89ee875fb705c
Lisa Carlson <lisa.carlson85@gmail.com>
2015-10-21 21:47:20 -0600

    Render form preview and open options tab

--- myApp/public/javascripts/form-builder.js
+++ myApp/public/javascripts/form-builder.js
@@ -1,5 +1,6 @@
 $( document ).ready(function() {
   var form = {
+    id: "new",
     title: "My Form",
     components: [],
     submitButton: {
@@ -15,54 +16,72 @@ $( document ).ready(function() {
 
   function renderForm() {
     var $container = $("#form-preview");
-    var drobbapleSection = "<div class='row'><div class='col-md-12 droppable-section ui-droppable'></div></div>"; 
+    var droppableSection = "<div class='row'><div class='col-md-12 droppable-section ui-droppable'></div></div>"; 
     var submitButton = form.submitButton;
     var cancelButton = form.cancelButton;
     var components = form.components;
     $container.html('');
     console.log($container);
-    $container.append("<form class='fb-form'></form>");
+    $container.append("<form data-form-id='" + form.id + "' class='fb-form'></form>");
     $form = $container.find('.fb-form');
     $form.append("<h2>" + form.title + "</h2>");
-    $form.append(drobbapleSection);
+    $form.append(droppableSection);
     for(x in components){
       var component = components[x];
       switch(component.type){
         case "text":
-          $form.append("<div class='form-group fb-input-text'><label for='" + component.name + "'>" + component.label + "</label><input class='form-control' type='text' placeholder='" + component.placeholder + "' name='" + component.name + "' value='" + component.value + "' /></div>");
+          $form.append("<div data-component-id='" + component.id + "' class='form-group fb-input-text'><label for='" + component.name + "'>" + component.label + "</label><input class='form-control' type='text' placeholder='" + component.placeholder + "' name='" + component.name + "' value='" + component.defaultValue + "' /></div>");
           break;
         case "email":
-          $form.append("<div class='form-group fb-input-email'><label for='" + component.name + "'>" + component.label + "</label><input class='form-control' type='text' placeholder='" + component.placeholder + "' name='" + component.name + "' value='" + component.value + "' /></div>");
+          $form.append("<div data-component-id='" + component.id + "' class='form-group fb-input-email'><label for='" + component.name + "'>" + component.label + "</label><input class='form-control' type='text' placeholder='" + component.placeholder + "' name='" + component.name + "' value='" + component.defaultValue + "' /></div>");
           break;  
         default:
       }
     }
-    $form.append(drobbapleSection);
-    $form.append("<div class='row'><div class='col-md-12 fb-buttons'><button type='reset' class='" + cancelButton.classes + "'>" + cancelButton.label + "</button><button type='reset' class='" + submitButton.classes + "'>" + submitButton.label + "</button></div>");
+    $form.append(droppableSection);
+    $form.append("<div class='row'><div class='col-md-12 fb-buttons'><button type='reset' class='" + cancelButton.classes + "'>" + cancelButton.label + "</button><button type='submit' class='" + submitButton.classes + "'>" + submitButton.label + "</button></div>");
     initDroppable();
   }
 
-  function initDroppable(){
+  function renderOptions(component) {
+    $('a[href="#options"]').tab('show');
+    $('#options-reset').click();
+    var $optionsForm = $("#options form"); 
+    $optionsForm.attr('data-component-id', component.id);
+    $optionsForm.find('#component-label').val(component.label);
+    $optionsForm.find('#component-name').val(component.name);
+    $optionsForm.find('#component-placeholder').val(component.placeholder);
+    $optionsForm.find('#component-default-value').val(component.defaultValue);
+    var $required = $optionsForm.find('#component-required');
+    if(component.required){
+      $required.prop('checked', true);
+    }
+  }
 
+  function initDroppable(){
     $(".droppable-section").droppable({
       accept: ".draggable",
       hoverClass: "drop-hover",
       drop: function(event, ui) {
-          $(this).droppable('disable');
-          $('ul.nav.nav-tabs').find('.comp').removeClass('active');
-          $('ul.nav.nav-tabs').find('.comp-options').addClass('active');
-          var $target = $(ui.draggable);
-          var type = $target.attr('data-type');
-          var component = {
-            type: type,
-            label: 'Input ' + form.components.length + 1,
-            placeholder: '',
-            name: 'input' + form.components.length + 1,
-            value: ''
-          };
+        $(this).droppable('disable');
+        $('ul.nav.nav-tabs').find('.comp').removeClass('active');
+        $('ul.nav.nav-tabs').find('.comp-options').addClass('active');
+        var $target = $(ui.draggable);
+        var type = $target.attr('data-type');
+        var number = form.components.length + 1;
+        var component = {
+          id: form.id + "-" + number,
+          type: type,
+          label: 'Input ' + number,
+          name: 'input' + number,
+          placeholder: '',
+          defaultValue: '',
+          required: false
+        };
 
-          form.components.push(component);
-          renderForm();
+        form.components.push(component);
+        renderForm();
+        renderOptions(component);
       }
     });
   }
@@ -75,13 +94,7 @@ $( document ).ready(function() {
     start: function(e, ui) {
       $(ui.helper).addClass("ui-draggable-helper");
     },
-  });
-  // $(".draggable-no-clone").draggable({
-  //   revert: "invalid",
-  //   start: function(e, ui) {
-  //     $(ui.helper).addClass("ui-draggable-helper");
-  //   },
-  // });
+  }); 
   $(".btn.draggable").draggable({
     cancel: false,
     revert: "invalid",
